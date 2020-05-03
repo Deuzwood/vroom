69e509e8ad70e8a6036f0c34caec2572f44206e5
Deuzwood <nicolaswallet@hotmail.fr>
2020-05-03 18:47:46 +0200

    Add tests

--- public/js/env_test.js
+++ public/js/env_test.js
@@ -3,15 +3,16 @@ last =  p.pop()
 
 let carte;
 if(last=='tree'){
-    for(let i=0; i<100;i+=10)
-        for(let j=0;j<100;j+=10)
+    for(let i=-100; i<=100;i+=10)
+        for(let j=-100;j<=100;j+=10)
             tree(i,0,j)   
 }else if(last == 'bush'){
-    for(let i=0; i<100;i+=10)
-        for(let j=0;j<100;j+=10)
+    for(let i=-100; i<=100;i+=10)
+        for(let j=-100;j<=100;j+=10)
             bush(i,0,j)
 
 }else if(last == 'forest'){
     forest(new THREE.Vector3(0,0,0),30)
     forest(new THREE.Vector3(80,0,80),20)
+    forest(new THREE.Vector3(-100,0,-100),50)
 }
