7987c514abd6ffd20bf2325ba3219a7aff76eb20
Deuzwood <nicolaswallet@hotmail.fr>
2020-05-03 19:15:29 +0200

    Add lake

--- public/js/terrain.js
+++ public/js/terrain.js
@@ -4,28 +4,18 @@ let geometry
 let first=true;
 let test_noise;
 let gen;
-function terrain(min,max,box=0){
+function terrain(min,max,lac=false){
 
-    
     map = new THREE.Box3(new THREE.Vector3(20,0,20), new THREE.Vector3(40,4,40))
 //var maph = new THREE.Box3Helper( map, 0xffff00 );
 //scene.add( maph );
 
-if (box==0)
+
+
 box = map;
 
-
-  geometry_w = new THREE.PlaneGeometry( max.x-min.x, max.z-min.z, 50, 50 );
-    geometry_w.rotateX(THREE.Math.degToRad(-90))
-  
-    const water = new THREE.Mesh(geometry_w, new THREE.MeshPhongMaterial({
-        color:0x00ffff,
-        opacity:0.5,
-        transparent:true
-    }))
-    water.position.y-=1
-    scene.add(water)
-
+const SIZE_PLUS = 100
+ 
     gen = new THREE.Box3();
     AABB_road.forEach( box => {
         //var helper = new THREE.Box3Helper( box, 0xff00ff );
@@ -37,13 +27,28 @@ box = map;
    // var helper = new THREE.Box3Helper( gen, 0xff00000 );
         //scene.add( helper );
 
+    if(lac){
+        var lac_helper = new THREE.Box3Helper( lac, 0xffff00 );
+        scene.add( lac_helper );
 
-        const SIZE_PLUS = 100
-        geometry = new THREE.PlaneGeometry( gen.max.x-gen.min.x+SIZE_PLUS, gen.max.z-gen.min.z+SIZE_PLUS, (gen.max.x-gen.min.x+SIZE_PLUS)/10 , (gen.max.z-gen.min.z+SIZE_PLUS)/10 );
-     
-     geometry.rotateX(THREE.Math.degToRad(-90))
+        geometry_w = new THREE.PlaneGeometry( gen.max.x-gen.min.x+SIZE_PLUS, gen.max.z-gen.min.z+SIZE_PLUS, (gen.max.x-gen.min.x+SIZE_PLUS)/10 , (gen.max.z-gen.min.z+SIZE_PLUS)/10 );
+        geometry_w.rotateX(THREE.Math.degToRad(-90))
+        
+        const water = new THREE.Mesh(geometry_w, new THREE.MeshPhongMaterial({
+            color:0x00ffff,
+            opacity:0.5,
+            transparent:true
+        }))
+        water.position.y-=1
+        geometry_w.translate(gen.getCenter().x,-0.2,gen.getCenter().z)
+        scene.add(water)
+    }
 
-        geometry.translate(gen.getCenter().x,-0.2,gen.getCenter().z)
+
+
+    geometry = new THREE.PlaneGeometry( gen.max.x-gen.min.x+SIZE_PLUS, gen.max.z-gen.min.z+SIZE_PLUS, (gen.max.x-gen.min.x+SIZE_PLUS)/10 , (gen.max.z-gen.min.z+SIZE_PLUS)/10 );
+    geometry.rotateX(THREE.Math.degToRad(-90))
+    geometry.translate(gen.getCenter().x,-0.2,gen.getCenter().z)
     for(let i=geometry.vertices.length-1;i>0;i--){
      //   geometry.vertices[i].y=0
 
@@ -61,7 +66,13 @@ box = map;
         }
         if(gen.containsPoint(geometry.vertices[i]) && m!=0)
             geometry.vertices[i].y=m/2-Math.random()*5+Math.log(m)
-       
+        if(lac)
+            if(lac.containsPoint(geometry.vertices[i]) && m!=0){
+                geometry.vertices[i].y=-(+Math.random()*5+Math.log(m))
+                geometry_w.vertices[i].y=-Math.random()
+          
+            }
+              
           
     }
 
