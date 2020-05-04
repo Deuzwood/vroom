29d6dbfe7d754d9ac63ddc229224aa8f3a6b11d4
Deuzwood <nicolaswallet@hotmail.fr>
2020-05-05 01:30:30 +0200

    rm comment

--- public/js/seg_test.js
+++ public/js/seg_test.js
@@ -55,27 +55,4 @@ switch (last) {
 		break;
 }
 
-/*
-let test= [ 
-    new Segment('road', 'linear',new THREE.Vector3(0,0,0), new THREE.Vector3(100,0,0)),
-	new Segment('road', 'linear',new THREE.Vector3(-50,0,0), new THREE.Vector3(-50,0,100)),
-	new Segment('road', 'linear',new THREE.Vector3(20,0,20), new THREE.Vector3(80,0,80)),
-	new Segment('road','linear',new THREE.Vector3(-20,0,-20),new THREE.Vector3(80,10,-20)),
-	new Segment('road','linear',new THREE.Vector3(-40,10,-40),new THREE.Vector3(50,30,-60)),
-
-	new Segment('road', 'intersect', new THREE.Vector3(-40,0,-60), [1,1,0,0]),
-	new Segment('road', 'intersect', new THREE.Vector3(-60,0,-60), [1,1,1,1]),
-	new Segment('road', 'intersect', new THREE.Vector3(-80,0,-60), [0,0,0,0]),
-	new Segment('road', 'intersect', new THREE.Vector3(-100,0,-60), [0,1,1,0]),
-	
-	
-	new Segment('road','curve',new THREE.Vector3(40,0,-40),[1,'x']),
-	new Segment('road','curve',new THREE.Vector3(60,0,-40),[2,'x']),
-	new Segment("road",'linear',new THREE.Vector3(60,0,-30), new THREE.Vector3(60,0,-40)),
-	new Segment("road",'linear',new THREE.Vector3(40,0,-30), new THREE.Vector3(40,0,-40)),
-
-	//new Segment('road', 'curve_from_to', new new THREE.Vector3(0,0,0), new THREE.Vector3(100,0,100) )
-
-];*/
-
 value.forEach( segment => segment.render());
